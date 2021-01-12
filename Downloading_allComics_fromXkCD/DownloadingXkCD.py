2152b85f4c83dfb7355f9b8045737527e4db5216
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2021-01-13 00:46:21 +0600

    Web_scraping_based

---
+++ Downloading_allComics_fromXkCD/DownloadingXkCD.py
@@ -0,0 +1,31 @@
+import requests , os , bs4
+
+url = "http://xkcd.com"
+os.makedirs("xkcd" , exist_ok = True)
+
+while not url.endswith("#"):
+	print("Downloading pages %s..." % url)
+	res = requests.get(url)
+	res.raise_for_status()
+
+	soup = bs4.BeautifulSoup(res.text)
+
+	comicElem = soup.select("#comic img")
+	if comicElem == []:
+		print("Couldn't found the image")
+	else:
+		comicURL = "http:" + comicElem[0].get("src")
+		print("Downloading Images %s..." % (comicURL))
+		res = requests.get(comicURL)
+		res.raise_for_status()
+
+		imageFile = open(os.path.join("xkcd" , os.path.basename(comicURL)) , "wb")
+		for chunk in res.iter_content(100000):
+			imageFile.write(chunk)
+		imageFile.close()
+
+
+	prevLink = soup.select('a[rel="prev"]')[0]
+	url = "http://xkcd.com" + prevLink.get("href")
+
+print("Done.")
