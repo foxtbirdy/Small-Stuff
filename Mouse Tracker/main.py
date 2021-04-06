f04a8134d1ae74a89deeee9469f521dfa7c00bef
Climax_ <72243209+Code-Blender-7@users.noreply.github.com>
2021-04-06 23:30:08 +0600

    added time and greetings?

--- Mouse Tracker/main.py
+++ Mouse Tracker/main.py
@@ -1,5 +1,22 @@
 import  pyautogui
 import time
+
+def greetings():
+    print("Please press Crtl + C to quit..")
+    time.sleep(3)
+    print("Mouse Tracker beginning in 5 seconds...")
+    time.sleep(1)
+    print("4")
+    time.sleep(1)
+    print("3")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("1")
+    time.sleep(1)
+ 
+greetings()
+
 try:
     while True:
         x,y = pyautogui.position()
