6713a082952684062651b0b9a0b87e9c091e037a
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2021-11-04 11:37:06 +0600

    added new test functions, completed stress test

--- Convert JPG to PNG/script.py
+++ Convert JPG to PNG/script.py
@@ -2,27 +2,43 @@
 # @Author: Climax
 # @Date:   2021-11-03 19:59:47
 # @Last Modified by:   Climax
-# @Last Modified time: 2021-11-04 00:50:10
+# @Last Modified time: 2021-11-04 11:35:50
 
 import os
 import sys
-from PIL import Image, ImageFilter
+from PIL import Image
 
 
-for file in os.listdir(os.path.dirname(sys.argv[0])):
+files_detected = 0  # files that are found
+files_converted = 0  # files that are converted
+files_skipped = 0  # files that does not fit format condition
+
+target_directory = os.listdir(
+    os.path.dirname(sys.argv[0])
+)  # address to script's directory
+
+saved_img_folder = "wipeout"
+
+
+def saving_images(target_file, saving_directory):
+    try:
+        os.mkdir(saving_directory)
+    except FileExistsError:
+        print("File converting => ", target_file)
+        img = Image.open(target_file)
+        img.save(f"{saving_directory}/{os.path.splitext(target_file)[0]}.png")
+
+
+for file in target_directory:
+    files_detected += 1
     if file == os.path.basename(sys.argv[0]):  # we don't want our file to get edited
         pass
     elif os.path.splitext(file)[1] != ".jpg":  # check if it's a jpg
+        files_skipped += 1
         pass
     else:
-        print(file)
-        try:
-            os.mkdir("converted files")
-        except FileExistsError:
-            print("Folder already exists")
-            img = Image.open(file)
-            img.save(f"converted files/{os.path.splitext(file)[0]}.png")
-
+        files_converted += 1
+        saving_images(target_file=file, saving_directory=saved_img_folder)
 
 # Find all the JPG files
 
@@ -31,7 +47,9 @@ for file in os.listdir(os.path.dirname(sys.argv[0])):
 # save the files in that folder
 
 print(os.listdir)
-
+print(files_converted)
+print(files_skipped)
+print(files_detected)
 
 # def folderCreation(fileName):
 #   if os.
