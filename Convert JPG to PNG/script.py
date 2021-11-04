6ce1ed1d1ba4c384f74f3890e6ba30ab61bb0dae
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2021-11-05 00:04:25 +0600

    added features, removed unnecessary codelines, added test files

--- Convert JPG to PNG/script.py
+++ Convert JPG to PNG/script.py
@@ -2,16 +2,17 @@
 # @Author: Climax
 # @Date:   2021-11-03 19:59:47
 # @Last Modified by:   Climax
-# @Last Modified time: 2021-11-04 11:35:50
+# @Last Modified time: 2021-11-04 22:36:34
 
 import os
 import sys
+import PIL
 from PIL import Image
-
 
 files_detected = 0  # files that are found
 files_converted = 0  # files that are converted
 files_skipped = 0  # files that does not fit format condition
+files_corrupted = 0
 
 target_directory = os.listdir(
     os.path.dirname(sys.argv[0])
@@ -29,28 +30,30 @@ def saving_images(target_file, saving_directory):
         img.save(f"{saving_directory}/{os.path.splitext(target_file)[0]}.png")
 
 
+def save_log(*args):
+    with open(f"{saved_img_folder}/Script Log.txt", mode="w") as log_file:
+        log_file.write(f"Items Detected - {str(files_detected)} \n")
+        log_file.write(f"Images Converted - {str(files_converted)} \n")
+        log_file.write(f"Images Skipped - {str(files_skipped)} \n")
+        log_file.write(f"Images Found Corrupted - {str(files_corrupted)} \n\n")
+
+
 for file in target_directory:
-    files_detected += 1
-    if file == os.path.basename(sys.argv[0]):  # we don't want our file to get edited
+    try:
+        files_detected += 1
+        if file == os.path.basename(
+            sys.argv[0]
+        ):  # we don't want our file to get edited
+            pass
+        elif os.path.splitext(file)[1] != ".jpg":  # check if it's a jpg
+            files_skipped += 1
+            pass
+        else:
+            files_converted += 1
+            saving_images(target_file=file, saving_directory=saved_img_folder)
+            save_log(file)
+
+    except PIL.UnidentifiedImageError:
+        print("File", file, "failed to convert")
+        files_corrupted += 1
         pass
-    elif os.path.splitext(file)[1] != ".jpg":  # check if it's a jpg
-        files_skipped += 1
-        pass
-    else:
-        files_converted += 1
-        saving_images(target_file=file, saving_directory=saved_img_folder)
-
-# Find all the JPG files
-
-# Create a new folder
-
-# save the files in that folder
-
-print(os.listdir)
-print(files_converted)
-print(files_skipped)
-print(files_detected)
-
-# def folderCreation(fileName):
-#   if os.
-#
