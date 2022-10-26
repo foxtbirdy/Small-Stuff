f435b487386e4d57092291f2de82d3d4300e519a
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2022-10-26 18:54:13 +0600

    cleaned the code even futhur

--- Convert_JPG_to_PNG-[Rich-integrate]/program files/modifed_script.py
+++ Convert_JPG_to_PNG-[Rich-integrate]/program files/modifed_script.py
@@ -2,41 +2,46 @@
 # @Author: Climax
 # @Date:   2021-11-03 19:59:47
 # @Last Modified by:   Climax
-# @Last Modified time: 2022-10-26 15:40:52
+# @Last Modified time: 2022-10-26 18:53:08
 
 import os
 import sys
 import PIL
 from PIL import Image
 import time
+import datetime
 
-
-
+now = datetime.datetime.now()
 UserInput = input("Enter your directory here: ")
-target_directory_listdir = os.listdir(UserInput)  # address to script's directory
 
-def saving_images(selectedFiles,saving_directory="D:/Coding_Nerds/FTD/experiment01/M4A1 Arts/converted"):
+# folder with the current date
+saving_dir_default = f"{UserInput}/Converted[{now.strftime('%Y-%m-%d(%H.%M hours)')}]"
+
+
+
+def saving_images(file , saving_dir=saving_dir_default):
+    """
+    saving the file image
+    """
     
+    img = Image.open(f"{UserInput}/{file}")
+    img.save(f"{saving_dir}/{file}.png")
+    print("File converted => ", file)
 
 
-    for file in selectedFiles:
-        img = Image.open(f"{UserInput}/{file}")
-        img.save(f"{saving_directory}/{file}.png")
-        print("File converting => ", file)
 
 
 def script_Runtime():
-
-    for file in target_directory_listdir:
+    selectedFiles = []
+    for file in os.listdir(UserInput): # probe to given directory
         if os.path.splitext(file)[1] != ".jpg":  # check if it's a jpg
-            files_skipped += 1
             pass
         else:
-            files_detected += 1
             selectedFiles.append(file)
 
-    # dont loop. let the magic work.
-    saving_images(selectedFiles)
+    os.makedirs(saving_dir_default)
+    for file in selectedFiles:
+        saving_images(file)
 
 
 
