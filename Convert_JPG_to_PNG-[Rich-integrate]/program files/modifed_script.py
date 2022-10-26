3143f91f3edbdc997edb077caff9cd2cda2dfa6c
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2022-10-26 20:14:19 +0600

    added rich library

--- Convert_JPG_to_PNG-[Rich-integrate]/program files/modifed_script.py
+++ Convert_JPG_to_PNG-[Rich-integrate]/program files/modifed_script.py
@@ -2,31 +2,36 @@
 # @Author: Climax
 # @Date:   2021-11-03 19:59:47
 # @Last Modified by:   Climax
-# @Last Modified time: 2022-10-26 18:53:08
+# @Last Modified time: 2022-10-26 20:13:33
 
 import os
 import sys
 import PIL
 from PIL import Image
+from rich import print
+from rich.progress import Progress
 import time
 import datetime
 
+
 now = datetime.datetime.now()
 UserInput = input("Enter your directory here: ")
+
+
 
 # folder with the current date
 saving_dir_default = f"{UserInput}/Converted[{now.strftime('%Y-%m-%d(%H.%M hours)')}]"
 
 
 
-def saving_images(file , saving_dir=saving_dir_default):
+def saving_images(file , saving_dir):
     """
     saving the file image
     """
     
     img = Image.open(f"{UserInput}/{file}")
     img.save(f"{saving_dir}/{file}.png")
-    print("File converted => ", file)
+    print(f"[green]File converted[/] => {file}")
 
 
 
@@ -39,10 +44,25 @@ def script_Runtime():
         else:
             selectedFiles.append(file)
 
-    os.makedirs(saving_dir_default)
-    for file in selectedFiles:
-        saving_images(file)
 
+    userChoice = input("\nCreate a new directory? [y/n]: ")
+    if userChoice.lower() == "n":
+        os.makedirs(saving_dir_default) # creating a new directory
+        for file in selectedFiles:
+            saving_images(file, saving_dir_default)
+    elif userChoice.lower() == "y":
+        print("[cyan]This is not built yet")
 
 
 script_Runtime()
+
+
+
+"""
+LOGIC
+
+program takes main directory input file converting
+program turns all files into png.
+
+
+"""
